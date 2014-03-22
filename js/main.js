24827329f6fbdbb83f23ec1de8588fb706bae36d
developfor <will@developfor.com>
2014-03-22 18:32:27 -0400

    font color change

--- js/main.js
+++ js/main.js
@@ -22,7 +22,9 @@
 
       return d3.scale.ordinal()
       .domain(["male", "female", "female & male"])
-      .range(["#97BF3F", "#FF4500", "#F8D6A9"]);
+      // .range(["#97BF3F", "#FF4500", "#F8D6A9"]);
+      .range([ "#205081", "#24A6E8","#3B7FC4"]);
+
     }
 
     function addDollar(data){
